68135b1bc1656762cd3c2f17b4bb3b670dc089eb
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-03-03 21:12:56 +0600

    folder structure updated

--- template.py
+++ template.py
@@ -4,23 +4,25 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+project_name = "ChestCancerClassification"
+
 list_of_files = [
     ".github/workflows/.gitkeep",
-    "src/__init__.py",
-    "src/components/__init__.py",
-    "src/utils/__init__.py",
-    "src/config/__init__.py",
-    "src/config/configuration.py",
-    "src/pipline/__init__.py",
-    "src/entity/__init__.py",
-    "src/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
     "experiments/experiments.ipynb",
-    "template/index.html"
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
