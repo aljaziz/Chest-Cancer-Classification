6c805275e47cbe73af53445c3d16f0036b234688
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-03-05 23:43:28 +0600

    data ingestion component updated

--- src/ChestCancerClassification/components/data_ingestion.py
+++ src/ChestCancerClassification/components/data_ingestion.py
@@ -21,7 +21,6 @@ class DataIngestion:
             logger.info(
                 f"Downloading data from {dataset_url} into file {zip_download_dir}"
             )
-
             file_id = dataset_url.split("/")[-2]
             prefix = "https://drive.google.com/uc?/export=download&confirm=yTib&id="
             gdown.download(prefix + file_id, zip_download_dir)
@@ -41,3 +40,4 @@ class DataIngestion:
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, "r") as zip_ref:
             zip_ref.extractall(unzip_path)
+        os.remove(self.config.local_data_file)
