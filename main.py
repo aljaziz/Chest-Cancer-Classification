6e6dfd4b698b5b9dff097108adb46904a5d11848
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-03-10 16:49:17 +0600

    model evaluation added

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from ChestCancerClassification.pipeline.prepare_base_model_pipeline import (
 from ChestCancerClassification.pipeline.model_trainer_pipeline import (
     ModelTrainingPipeline,
 )
+from ChestCancerClassification.pipeline.model_evaluation_pipeline import (
+    EvaluationPipeline,
+)
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -42,6 +45,18 @@ try:
     model_trainer = ModelTrainingPipeline()
     model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Evaluation stage"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evalution = EvaluationPipeline()
+    model_evalution.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
     logger.exception(e)
     raise e
