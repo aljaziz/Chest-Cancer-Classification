c875818df4a2983ae6795b2bc287171f24918e7f
aljaziz <turjatheharbingerofdeath@gmail.com>
2024-03-10 18:12:45 +0600

    model evaluation added

--- src/ChestCancerClassification/entity/config_entity.py
+++ src/ChestCancerClassification/entity/config_entity.py
@@ -15,6 +15,9 @@ class PrepareBaseModelConfig:
     root_dir: Path
     base_model_path: Path
     updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
     params_weights: str
     params_classes: int
 
@@ -23,29 +26,19 @@ class PrepareBaseModelConfig:
 class TrainingConfig:
     root_dir: Path
     trained_model_path: Path
+    updated_base_model_path: Path
     training_data: Path
-    validation_data: Path
-    all_image_path: list
     params_epochs: int
-    params_weights: str
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
-    params_learning_rate: float
-    params_classes: int
-    params_device: str
 
 
 @dataclass(frozen=True)
 class EvaluationConfig:
     path_of_model: Path
-    testing_data: Path
+    training_data: Path
     all_params: dict
-    all_image_path: list
     mlflow_uri: str
     params_image_size: list
     params_batch_size: int
-    params_device: str
-    params_learning_rate: float
-    params_classes: int
-    params_weights: str
